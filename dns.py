10cb06f1eef6b83ee01d68ebcd5f53fe491110a6
DarkArneus <asola268@gmail.com>
2024-04-04 10:14:59 -0400

    Clear output

--- dns.py
+++ dns.py
@@ -1,4 +1,4 @@
-import os
+import os, re
 import requests
 
 wordlist_array = []
@@ -26,18 +26,11 @@ class DNS():
     def resolveRegulator(self):
         with open(os.path.join(self.folder_results,"clean_wildcards.txt"), 'r') as f:
             for wildcard in f:
-                print('puredns resolve {} -r {} --write {} | httpx -fr -sc -td -title -bp -silent -o {}' .format(
+                os.system('puredns resolve {} -r {} --write {}' .format(
                 os.path.join(self.folder_dns, "regulator_" + wildcard.strip()),
                 os.path.join(self.folder_results, "wordlists", "resolvers.txt".strip()),
-                os.path.join(self.folder_dns, "resolve_regulator" + wildcard.strip()),
-                os.path.join(self.folder_dns, "resolve_regulator_httpx_" + wildcard.strip())
-        ))
-                os.system('puredns resolve {} -r {} --write {} | httpx -fr -sc -td -title -bp -silent -o {}' .format(
-                os.path.join(self.folder_dns, "regulator_" + wildcard.strip()),
-                os.path.join(self.folder_results, "wordlists", "resolvers.txt".strip()),
-                os.path.join(self.folder_dns, "resolve_regulator" + wildcard.strip()),
-                os.path.join(self.folder_dns, "resolve_regulator_httpx_" + wildcard.strip())
-        ))
+                os.path.join(self.folder_dns, "resolve_regulator" + wildcard.strip())
+            ))
     
     def getWordlist(self):
         r = requests.get("https://raw.githubusercontent.com/infosec-au/altdns/master/words.txt", verify=False)
@@ -46,9 +39,24 @@ class DNS():
                 f.write(r.text)
 
     def resolvePureDns(self):
-        command = 'puredns resolve {} --write puredns_valids.txt | httpx -fr -sc -td -title -bp -silent -o puredns_httpx.txt' .format(
+        command = 'puredns resolve {} --write puredns_valids.txt' .format(
             os.path.join(self.folder_dns, "altdns_results.txt")
         )
+
+    def clearOutput(self):
+        # Verificar si el archivo all_dns_subdomains.txt ya existe
+        output_file = os.path.join(self.folder_dns, "all_dns_subdomains.txt")
+        if not os.path.exists(output_file):
+            # Si no existe, crear el archivo
+            open(output_file, mode='a').close()
+        # Limpiamos la mierda de arriba que creo para poder hacerle el anew
+        os.system('rm {}'.format(os.path.join(self.folder_dns, "regulator_*")))
+        pattern = re.compile(r"httpx", re.IGNORECASE)
+        for file in os.listdir(self.folder_dns):
+            match = pattern.search(file)
+            if not match:
+                os.system('cat {} | anew {}'.format(os.path.join(self.folder_dns, file),os.path.join(self.folder_dns,"all_dns_subdomains.txt")))
+        os.system('rm {}'.format(os.path.join(self.folder_dns, "resolve_*")))
 
     def getResolvers(self):
         r = requests.get("https://raw.githubusercontent.com/trickest/resolvers/main/resolvers.txt", verify=False)
